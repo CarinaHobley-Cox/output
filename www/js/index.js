c4846d4938d04ad4a09f2b70b9d032d7c481d303
CarinaHobley-Cox <hobc2_16@uni.worc.ac.uk>
2017-11-13 15:43:47 +0000

    added pluggin

--- www/js/index.js
+++ www/js/index.js
@@ -6,10 +6,38 @@ $(document).on("pagecreate","#pageone",function(){
 
 
 function submitText() {
-	var text = $('#textinput').val();
-	alert(text);
+	
+    var rand = random();
+    
+    
+    //print hello if true
+    
+    
+    // print bye if false
+    
+    
+    if(rand == true) {
+        
+      //  alert("Hello");
+         $("#resultText").text("True");
+    }
+    
+    else {
+        
+       // alert("Bye");
+         $("#resultText").text("False");
+    }
+    
+    
+    //$("#resultText").text("newText");
+    
+}
+
+ function random() {
+	return !Math.round(Math.random());
 }
 
 function storeValue(key, value) {
+    
 	//add some code to store the key-value pair in persistant storage 
 }
